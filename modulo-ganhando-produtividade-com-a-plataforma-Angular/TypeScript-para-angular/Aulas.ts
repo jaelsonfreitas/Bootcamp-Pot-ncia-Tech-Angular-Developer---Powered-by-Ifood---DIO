83ccc2f24e8417d693fc48ecafeb7db4b6f1711c
Jaelson Jose de Freitas <macjaelson@MacBook-Pro-de-Jaelson.local>
2023-05-04 21:36:06 -0300

    Tuplas.

---
+++ modulo-ganhando-produtividade-com-a-plataforma-Angular/TypeScript-para-angular/Aulas.ts
@@ -0,0 +1,14 @@
+/**
+ * dentro do type temos duas maneiras de declarar arrays
+ */
+
+let dados: string[] = ["Felipe", "Ana", "Jaelson"];
+let dados2: Array<string> = ["Felipe", "Ana", "Jaelson"];
+
+let infos: (string | number)[] = ["Jaelson", 355];
+
+/**
+ * Tuplas, aqui precisamos seguir a exata ordem que foi criado
+ */
+
+let boleto :[string, number, number] = ["Conta de água", 199,9, 3234565];
--- modulo-ganhando-produtividade-com-a-plataforma-Angular/TypeScript-para-angular/arrays.ts
+++
@@ -1,8 +0,0 @@
-/**
- * dentro do type temos duas maneiras de declarar arrays
- */
-
-let dados: string[] = ["Felipe", "Ana", "Jaelson"];
-let dados2: Array<string> = ["Felipe", "Ana", "Jaelson"];
-
-let infos: (string | number)[] = ["Jaelson", 355];
